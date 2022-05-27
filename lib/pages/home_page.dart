4d4a1fc4fd72b18ef46678da639a492226b801d3
captain-nemo-1 <86842909+captain-nemo-1@users.noreply.github.com>
2022-05-27 21:43:31 +0530

    Edit login_page.dart to display username

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
-  final String name = "Captain Nemo";
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +9,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Shop"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to the App! $name version $days"),
-        ),
-      ),
-      drawer: Drawer(),
+      body: Center(child: Container(child: Text("Welcome to the App!"))),
+      drawer: Drawer(), //hamburger icon displays an empty panel
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,60 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_simpleapp/utils/routes.dart';
 
-class LogInPage extends StatelessWidget {
+class LogInPage extends StatefulWidget {
+  @override
+  _LogInPageState createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  String name = "";
+
+  moveToHome(BuildContext context) {}
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/login_image.png",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Hello There!",
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.indigo[500],
-                  fontWeight: FontWeight.bold,
+          child: Form(
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/login_image.png",
+                  fit: BoxFit.cover,
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 16.0, horizontal: 32.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter Username", labelText: "Username"),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Enter Password", labelText: "Password"),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    ElevatedButton(
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                    )
-                  ],
+                SizedBox(
+                  height: 20.0,
                 ),
-              )
-            ],
+                Text(
+                  "Hello $name!", //returns greetings with username
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.indigo[500],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 32.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Username", //placeholder
+                          labelText: "Username",
+                        ),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true, //hides typed characters
+                        decoration: InputDecoration(
+                            hintText: "Enter Password", labelText: "Password"),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      ElevatedButton(
+                        child: Text("Login"),
+                        style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                        onPressed: () {
+                          Navigator.pushNamed(context,
+                              MyRoutes.homeRoute); //provides access to homepage
+                        },
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
